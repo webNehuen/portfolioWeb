351539488c0a9b0d9afe1433cfd736f444f1db15
webNehuen <nehuenanapoli@gmail.com>
2022-10-18 13:14:39 -0300

    act

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
