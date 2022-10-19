c72d5b2c3db24f420394e1aa3bb567d1b9634039
webNehuen <nehuenanapoli@gmail.com>
2022-10-19 20:38:31 -0300

    act

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './componentes/header/header.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
